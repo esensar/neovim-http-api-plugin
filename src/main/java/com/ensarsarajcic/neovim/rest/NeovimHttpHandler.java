87d049aa9426e0be4fe37c8b91b3b2b4b2d2a835
Ensar Sarajčić <es.ensar@gmail.com>
2022-06-08 15:59:35 +0200

    Add api-info POC endpoint

--- src/main/java/com/ensarsarajcic/neovim/rest/NeovimHttpHandler.java
+++ src/main/java/com/ensarsarajcic/neovim/rest/NeovimHttpHandler.java
@@ -1,6 +1,10 @@
 package com.ensarsarajcic.neovim.rest;
 
 import com.ensarsarajcic.neovim.java.api.NeovimApi;
+import com.ensarsarajcic.neovim.java.api.types.apiinfo.ApiInfo;
+import com.ensarsarajcic.neovim.java.api.util.ObjectMappers;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
@@ -9,16 +13,44 @@ import java.nio.charset.StandardCharsets;
 
 public final class NeovimHttpHandler implements HttpHandler  {
     private final NeovimApi neovim;
+    private final ObjectMapper mapper;
 
     public NeovimHttpHandler(NeovimApi neovim) {
         this.neovim = neovim;
+        this.mapper = ObjectMappers.defaultNeovimMapper();
     }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        System.err.println("Got request on " + exchange.getRequestURI().toString());
-        exchange.sendResponseHeaders(200, 0);
-        exchange.getResponseBody().write("Hello to you".getBytes(StandardCharsets.UTF_8));
-        exchange.close();
+        System.err.printf("Got request: %s %s%n", exchange.getRequestMethod(), exchange.getRequestURI());
+        switch (exchange.getRequestURI().getPath()) {
+            case "/api-info":
+                if (exchange.getRequestMethod().equals("GET")) {
+                    neovim.getApiInfo().thenAccept(info -> {
+                        try {
+                            var body = info.toString().getBytes(StandardCharsets.UTF_8);
+                            exchange.sendResponseHeaders(200, body.length);
+                            exchange.getResponseBody().write(body);
+                        } catch (JsonProcessingException e) {
+                            e.printStackTrace();
+                            try {
+                                exchange.sendResponseHeaders(500, -1);
+                            } catch (IOException ex) {
+                                throw new RuntimeException(ex);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            throw new RuntimeException(e);
+                        } finally {
+                            exchange.close();
+                        }
+                    });
+                } else {
+                    exchange.sendResponseHeaders(405, -1);
+                }
+                break;
+            default:
+                exchange.sendResponseHeaders(404, -1);
+        }
     }
 }
